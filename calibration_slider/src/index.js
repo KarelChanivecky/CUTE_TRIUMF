987469f3306b87165966c1480b5c1ba0b39a761f
Sean <sean.joseph.green@gmail.com>
2021-02-15 19:16:12 -0800

    Re-injected previous branch code

--- calibration_slider/src/index.js
+++ calibration_slider/src/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import Slider from '@material-ui/core/Slider';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function getMarks() {
+   let marks = []
+   for (let i = 0, m = -10; i < 17; ++i, m += 10) {
+      marks[i] = {
+         value: m,
+         label: '' + m + '',
+      }
+   }
+   return marks;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function getOrientation() {
+   let orient = "horizontal";
+   if (window.innerWidth < window.innerHeight) {
+      orient = "vertical";
+   }
+   return orient;
+}
+
+function getCalibDivClass() {
+   return (window.innerWidth < window.innerHeight) ? "calib_control_vertical" : "calib_control_horizontal";
+}
+
+function CalibrationSlider(props) {
+   return (      
+      <div className={getCalibDivClass()} id="calibration_widget">
+      <div id="calibration_Slider"/>
+         <Slider
+         defaultValue={80}
+         orientation={getOrientation()}
+         // getAriaValueText={valuetext}
+         aria-labelledby="discrete-slider-always"
+         step={1}
+         marks={getMarks()}
+         min={-10}
+         max={150}
+         valueLabelDisplay="on"
+         />
+      </div>
+   );
+}
+
+//testing resizing rerender
+function CalibrationControl() {
+   const [dimensions, setDimensions] = React.useState({ 
+      height: window.innerHeight,
+      width: window.innerWidth
+   })
+   
+   React.useEffect(() => {
+   function handleResize() {
+      setDimensions({
+         height: window.innerHeight,
+         width: window.innerWidth
+      })
+   }
+
+   window.addEventListener('resize', handleResize)
+
+   return _ => {
+      window.removeEventListener('resize', handleResize)
+   }
+   })
+   return (
+         <CalibrationSlider />
+   );
+}
+
+ReactDOM.render(<CalibrationControl />, document.getElementById("root"));
