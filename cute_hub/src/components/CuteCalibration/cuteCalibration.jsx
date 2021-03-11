e79ca57fa689119f288f262f045ef6497c9ce4e3
Sean <sean.joseph.green@gmail.com>
2021-03-11 08:03:38 -0800

    Tidyied up and commented calibration code, css'd the slider

--- cute_hub/src/components/CuteCalibration/cuteCalibration.jsx
+++ cute_hub/src/components/CuteCalibration/cuteCalibration.jsx
@@ -4,11 +4,15 @@ import './cuteCalibration.css';
 // import {source_position, move_source} from './test.js'
 import Slider from '@material-ui/core/Slider';
 import Button from '@material-ui/core/Button';
-import { makeStyles, OutlinedInput, TextField, withStyles } from '@material-ui/core';
+import { makeStyles, OutlinedInput, TextField, ThemeProvider, Typography, withStyles } from '@material-ui/core';
 
 ////////////////////////////////////////////////////////// Testing Data Source ////////////////////////////////////////////////////////
+// The source postion slider looks at this value and adjusts according to it
 var source_position = 40;
 
+var moving = false;
+
+// This function is called when the move button is clicked and can be replaced with something that actually moves the slider.
 function move_source(pos) {
    setTimeout(()=>{
       if (pos > source_position) {   
@@ -23,7 +27,7 @@ function move_source(pos) {
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+// This creates an array of JSON objects that will be called at line ## to mark the values of a slider
 const marks = ()=>{
    let marks = []
    for (let i = 0, m = -10; i < 17; ++i, m += 10) {
@@ -35,22 +39,26 @@ const marks = ()=>{
    return marks;
 }
 
+// This sets the class of the div the slider is sitting in, 
+// the classes width and heights are found in the cuteCalibration.css file
 function getCalibDivClass() {
    return (window.innerWidth < window.innerHeight) ? "calib_control_vertical" : "calib_control_horizontal";
 }
 
-const StyledSlider = withStyles({
+// This JSON object sets the style properties of the sliders
+const sliderStyles = {
    root: {
-      color: '#52af77',
+      color: 'primary',
       height: 8,
       width: 860,
    },
    vertical: {
-      color: '#52af77',
+      color: 'primary',
       width: 8,
       height: 800,
       '& $rail': {
-        width: 8
+         marginLeft: -8,
+         width: 20
       },
       '& $track': {
         width: 8
@@ -64,6 +72,7 @@ const StyledSlider = withStyles({
    thumb: {
       height: 20,
       width: 20,
+      borderRadius: 0,
       backgroundColor: 'currentColor',
       border: '1px solid currentColor',
       marginTop: -8,
@@ -82,11 +91,17 @@ const StyledSlider = withStyles({
       borderRadius: 4,
    },
    rail: {
-      height: 8,
+      marginTop: -8,
+      height: 20,
       borderRadius: 4,
    },
-})(Slider);
+}
 
+// Creates a styled slider object that can be called like a regular slider with <StyledMovementSlider/>
+// Used in the return value of CalibrationSlider
+const StyledMovementSlider = withStyles(sliderStyles)(Slider);
+
+// Sets the styles of the move button.
 const buttonStyle = makeStyles({
    root:{
       maxwidth : 100,
@@ -108,8 +123,19 @@ function SourcePositionSlider(props) {
        return () => clearInterval(secTimer);
    });
 
+   // Applies some extra style to the source slider, starting with the original sliderStyle object.
+   let sourceStyles = sliderStyles;
+   sourceStyles.root.color = '#52af77';
+   sourceStyles.vertical.color = '#52af77';
+   sourceStyles.rail.display = 'none';
+   sourceStyles.thumb.marginTop = -66;
+   sourceStyles.vertical["& $thumb"].marginLeft = -56;
+
+   // Creates a new Slider object with specific styling
+   const StyledSourceSlider = withStyles(sourceStyles)(Slider);
+
    return (
-      <StyledSlider
+      <StyledSourceSlider
                orientation={props.orientation}
                value={srcPos}
                min={-10}
@@ -118,6 +144,7 @@ function SourcePositionSlider(props) {
             />
    )
 }
+
 
 function CalibrationSlider(props) {
    // First value represents current position, Second Value represents desired position
@@ -140,7 +167,7 @@ function CalibrationSlider(props) {
          <div  
             className={getCalibDivClass()}
             id="calibration_slider">
-            <StyledSlider
+            <StyledMovementSlider
                value={values[1]}
                orientation={orientation()}
                aria-labelledby="range-slider"
@@ -149,11 +176,11 @@ function CalibrationSlider(props) {
                // Steps controls the values the slider can have,
                // 1 means it will have values 1,2,3,4, etc.
                // .5 means it have have values .5,1,1.5,2
-               step={1}
+               step={0.1}
                /////////////////////////////
                min={-10}
                max={150}
-               valueLabelDisplay="on"
+               valueLabelDisplay="auto"
             />
             <SourcePositionSlider
                orientation={orientation()}
@@ -165,6 +192,7 @@ function CalibrationSlider(props) {
                id="calibration_input"
                value={values[1]} 
                type="number"
+               step={0.1}
                size='small'
                onChange={handleInputChange}
             />
@@ -180,7 +208,7 @@ function CalibrationSlider(props) {
    );
 }
 
-//testing resizing rerender
+// 
 export default function CalibrationControl(props) {
    const [dimensions, setDimensions] = React.useState({ 
       height: window.innerHeight,
