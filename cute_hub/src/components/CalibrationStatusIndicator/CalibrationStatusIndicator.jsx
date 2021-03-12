670c4651298b8925edeedc7d0645bc2b2bc90ed2
Gurden <gurdensingh@gmail.com>
2021-03-11 17:59:00 -0800

    system availability get live data

--- cute_hub/src/components/CalibrationStatusIndicator/CalibrationStatusIndicator.jsx
+++ cute_hub/src/components/CalibrationStatusIndicator/CalibrationStatusIndicator.jsx
@@ -21,62 +21,75 @@ function CalibrationStatusIndicator(props) {
 
     const classes = useStyles();
 
-    // return (
-    //     <h6 className={classes.color}>System available</h6>
-    // );
-
     const [show, setShow] = useState(false);
-    const [reserved, setReserved] = useState(false);
-    const [time, setTime] = useState("");
+    const [reserved, setReserved] = useState(false); //update it according to the data type of incoming data
+    const [time, setTime] = useState("few");
 
-  const handleClose = () => setShow(false);
-  const handleReserve = () => {
-      setReserved(true);
+    //TODO:  for the following function to work please update setReserved to whatever fields are coming as the response e.g. ".data" ?
+
+    // const getData = async () => {
+    //     try {
+    //         const fridge = await axios.get("http://localhost/echo_det_state.php")
+    //         console.log(fridge.data)
+    //         setReserved(fridge.data);  // set State
+
+    //     } catch (err) {
+    //         console.error(err.message);
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     getData()
+
+    //     const interval = setInterval(() => {
+    //         getData()
+    //     }, 10000) //updating every 10 seconds
+
+
+    //     return () => clearInterval(interval)
+    // }, [])  // includes empty dependency array
+
+
+
+    const handleClose = () => setShow(false);
+    const handleReserve = () => {
+        setReserved(true);
     }
-  const handleShow = () => setShow(true);
-  const title = reserved ? "System Unavailable" : "System Available";
-  const color = reserved ? "warning" : "success";
-  const subtext = reserved ? "System reserved for: " : "Reserve system for: ";
-  const time_hrs = reserved ? time + " Hours" : 
-  <FormControl
-  placeholder="Hours"
-  aria-label="Hours"
-  aria-describedby="basic-addon2"
-  onChange={e => setTime(e.target.value)}
-  type="text"
-/>;
-  const reserve_btn = reserved ? <></> : <Button variant="primary" onClick={handleReserve}>Reserve Now</Button>;
+    const handleShow = () => setShow(true);
+    const title = reserved == true ? "System Unavailable" : "System Available";
+    const color = reserved == true ? "warning" : "success";
+    const subtext = reserved == true ? "System reserved for " : "Reserve system for: ";
+    const time_hrs = reserved == true ? time + " hours" : 
+    <FormControl
+    placeholder="Hours"
+    aria-label="Hours"
+    aria-describedby="basic-addon2"
+    onChange={e => setTime(e.target.value)}
+    type="text"
+    />;
+    const reserve_btn = reserved ? <></> : <Button variant="primary" onClick={handleReserve}>Reserve Now</Button>;
 
-  return (
-    <>
-      <Button className={classes.color} variant={color} onClick={handleShow}>
-        {title}
-      </Button>
+    return (
+        <>
+        <Button className={classes.color} variant={color} onClick={handleShow}>
+            {title}
+        </Button>
 
-      <Modal show={show} onHide={handleClose} animation={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>System Availability</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{subtext} {time_hrs}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          {reserve_btn}
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+        <Modal show={show} onHide={handleClose} animation={false}>
+            <Modal.Header closeButton>
+            <Modal.Title>System Availability</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{subtext} {time_hrs}
+            </Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+                Close
+            </Button>
+            {reserve_btn}
+            </Modal.Footer>
+        </Modal>
+        </>
+    );
 }
-
-
-// function CalibrationStatusIndicator(props) {
-//     const alertText = "System available";
-//     const alertSeverity = "success";
-//     return (
-//         <Alert variant="filled" severity={alertSeverity}>{alertText}</Alert>
-//     );
-// }
 
 export default CalibrationStatusIndicator;
